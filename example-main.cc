449d7b07ce39f2ca45d4c075a0b4170f699d3e87
JY-Liu-i <1359981767@qq.com>
2021-02-22 13:10:26 +0800

    feat:命令行传参

--- example-main.cc
+++ example-main.cc
@@ -11,78 +11,82 @@
 #include "EvolutionApplication.h"
 #include "GroupInitializer.h"
 #include "Test.h"
+#include "string"
 using namespace ns3;
+using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("EvolutionpplicationExample");
 
-
-
-
 int main (int argc, char *argv[])
 {
+    string testCase = "test";
+    string tclFilePath = "scratch/ns3-vehicle-group-simulation/sumofiles/test.tcl";
+    
     CommandLine cmd;
+    cmd.AddValue("testCase", "", testCase);
+    cmd.AddValue("tclFilePath", "要加载的tcl文件位置", tclFilePath);
     cmd.Parse (argc, argv);
     
-    TestVGTreeHelper();
-    TestGroupInitialer();
+    uint32_t nNodes = 0;//节点数目
+    double simTime = 0; //仿真时间
+    
+    if(testCase=="test"){
+        nNodes = 4;
+        simTime = 60;
+        tclFilePath = "scratch/ns3-vehicle-group-simulation/sumofiles/test.tcl";
+    }
+    
+    cout<<"testCase: "<< testCase <<endl;
+    cout<<"tclFilePath: "<< tclFilePath <<endl;
+    cout<<endl;
+    
 
-//    uint32_t nNodes = 2;//节点数目
-//    double simTime = 5; //仿真时间
+    NodeContainer nodes;
+    nodes.Create(nNodes);
 
-//    NodeContainer nodes;
-//    nodes.Create(nNodes);
+    LogComponentEnable ("EvolutionApplication", LOG_LEVEL_FUNCTION);
+    
+    //使用NS3的移动模型，可以修改为SUMO的FCD输出
+    Ns2MobilityHelper mobility(tclFilePath);
+    mobility.Install(nodes.Begin(),nodes.End());
 
-//    LogComponentEnable ("EvolutionApplication", LOG_LEVEL_FUNCTION);
-//      
-//    //使用NS3的移动模型，可以修改为SUMO的FCD输出
-////    Ns2MobilityHelper mobility("/home/ljy/ns-allinone-3.30/ns-3.30/scratch/simpleWAVEAppliation/mobility.tcl");
-////    mobility.Install(nodes.Begin(),nodes.End());
-//    MobilityHelper mobility;
-//    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-//    positionAlloc->Add (Vector (1, 2, 3));
-//    positionAlloc->Add (Vector (1, 0.0, 0.0));
-//    mobility.SetPositionAllocator (positionAlloc);
-//    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-//    mobility.Install (nodes);
-//  
+    // The below set of helpers will help us to put together the wifi NICs we want
+    YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+    YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+    Ptr<YansWifiChannel> channel = wifiChannel.Create ();
+    wifiPhy.SetChannel (channel);
+  
+    //可以调节通信距离
+//    wifiPhy.Set ("TxPowerStart", DoubleValue (20) );
+//    wifiPhy.Set ("TxPowerEnd", DoubleValue (40) );
 
-//    // The below set of helpers will help us to put together the wifi NICs we want
-//    YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
-//    YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
-//    Ptr<YansWifiChannel> channel = wifiChannel.Create ();
-//    wifiPhy.SetChannel (channel);
-//  
-//    //可以调节通信距离
-////    wifiPhy.Set ("TxPowerStart", DoubleValue (20) );
-////    wifiPhy.Set ("TxPowerEnd", DoubleValue (40) );
+  // ns-3 supports generate a pcap trace
+    wifiPhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11);
+    NqosWaveMacHelper wifi80211pMac = NqosWaveMacHelper::Default ();
+    Wifi80211pHelper wifi80211p = Wifi80211pHelper::Default ();
 
-//  // ns-3 supports generate a pcap trace
-//    wifiPhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11);
-//    NqosWaveMacHelper wifi80211pMac = NqosWaveMacHelper::Default ();
-//    Wifi80211pHelper wifi80211p = Wifi80211pHelper::Default ();
+    wifi80211p.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                      "DataMode",StringValue ("OfdmRate6MbpsBW10MHz"),
+                                      "ControlMode",StringValue ("OfdmRate6MbpsBW10MHz"));
+    NetDeviceContainer devices = wifi80211p.Install (wifiPhy, wifi80211pMac, nodes);
 
-//    wifi80211p.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-//                                      "DataMode",StringValue ("OfdmRate6MbpsBW10MHz"),
-//                                      "ControlMode",StringValue ("OfdmRate6MbpsBW10MHz"));
-//    NetDeviceContainer devices = wifi80211p.Install (wifiPhy, wifi80211pMac, nodes);
+    //为节点添加应用
+    for (uint32_t i=0; i<nodes.GetN(); i++)
+    {
+        Ptr<EvolutionApplication> app_i = CreateObject<EvolutionApplication>();
+        app_i->SetStartTime (Seconds (0));
+        app_i->SetStopTime (Seconds (simTime));
+        nodes.Get(i)->AddApplication (app_i);
+    }
 
-//    //为节点添加应用
-//    for (uint32_t i=0; i<nodes.GetN(); i++)
-//    {
-//        Ptr<EvolutionApplication> app_i = CreateObject<EvolutionApplication>();
-//        app_i->SetStartTime (Seconds (0));
-//        app_i->SetStopTime (Seconds (simTime));
-//        nodes.Get(i)->AddApplication (app_i);
-//    }
+    Simulator::Stop(Seconds(simTime));
+  
+    //netAnim可视化
+//    AnimationInterface anim("EvolutionApplication.xml");
+//    anim.SetMobilityPollInterval (Seconds (1));
+  
+    Simulator::Run();
 
-//    Simulator::Stop(Seconds(simTime));
-//  
-//    //netAnim可视化
-////    AnimationInterface anim("EvolutionApplication.xml");
-////    anim.SetMobilityPollInterval (Seconds (1));
-//  
-//    Simulator::Run();
-
-//    Simulator::Destroy();
+    Simulator::Destroy();
     return 0;
 }
